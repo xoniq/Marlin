30add0a2f1ece58bb692d15b0606e77a636aab42
Jelle Posthuma <jelle@beyonit.nl>
2019-09-15 20:34:15 +0200

    Disable sensorless probing

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1973,7 +1973,7 @@
    * CAUTION: This could cause damage to machines that use a lead screw or threaded rod
    *          to move the Z axis. Take extreme care when attempting to enable this feature.
    */
-  #define SENSORLESS_PROBING // StallGuard capable drivers only
+  //#define SENSORLESS_PROBING // StallGuard capable drivers only
 
   #if EITHER(SENSORLESS_HOMING, SENSORLESS_PROBING)
     // TMC2209: 0...255. TMC2130: -64...63
