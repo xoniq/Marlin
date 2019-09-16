9a51372d0f406a53542433682d147ad793494c3e
Jelle Posthuma <jelle@beyonit.nl>
2019-09-16 20:27:08 +0200

    V3.3

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -133,7 +133,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "HCv3.0"
+#define CUSTOM_MACHINE_NAME "HCv3.3"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
